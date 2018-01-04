95c9c6197783a146ee4212a23ce888e530f5ab3b
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-01-04 09:56:55 -0500

    Removed commented out code

--- src/globals.js
+++ src/globals.js
@@ -1,15 +1,3 @@
-// let instance;
-//
-// class Singleton {
-// 	constructor () {
-// 		if (!instance) {
-// 			instance = new Map();
-// 		}
-//
-// 		return instance;
-// 	}
-// }
-
 /**
  * @name exports
  * @summary Simple map to store globals in
