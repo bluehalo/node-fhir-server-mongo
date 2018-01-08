2a0860c136e3fd5ce85e1d8785ac35ce326d30e4
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-01-08 09:34:41 -0500

    Added some mongo tests

--- server/src/services/observation/observation.service.test.js
+++ server/src/services/observation/observation.service.test.js
@@ -1,113 +1,75 @@
-const mockMongoClient = require('../../testutils/collection.mock');
+/* eslint-disable */
+const { COLLECTION, CLIENT, CLIENT_DB } = require('../../constants');
 const observationService = require('./observation.service');
 const mockLogger = require('../../testutils/logger.mock');
 const asyncHandler = require('../../lib/async-handler');
-const { CLIENT_DB } = require('../../constants');
+const { mongoConfig } = require('../../config');
+const mongoClient = require('../../lib/mongo');
 let globals = require('../../globals');
-
-let client;
 
 describe('Observation Service Test', () => {
 
-	beforeEach(() => {
-		client = mockMongoClient();
-		globals.set(CLIENT_DB, client);
+	beforeAll(async () => {
+
+		let [ err, client ] = await asyncHandler(
+			mongoClient(mongoConfig.connection, mongoConfig.options)
+		);
+
+		let db = client.db(mongoConfig.db_name);
+		let collection = db.collection(COLLECTION.OBSERVATION);
+
+		collection.drop();
+		await asyncHandler(collection.insert({ id: 1, type: 'observation' }));
+
+		globals.set(CLIENT, client);
+		globals.set(CLIENT_DB, db);
+
+	});
+
+	afterAll(() => {
+		let client = globals.get(CLIENT);
+		client.close();
 	});
 
 	describe('Method: getCount', () => {
 
-		test('should correctly pass back an error', async () => {
-			// Set our global client to error on it's next call
-			let message = 'Unable to connect to DB';
-			client.setError(message);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
-			let [ err, results ] = await asyncHandler(
-				observationService.getCount(null, mockLogger)
-			);
-
-			expect(results).toBeUndefined();
-			expect(err.message).toEqual(message);
-
-		});
-
 		test('should correctly pass back the count', async () => {
-			// Set the results for our global client
-			let count = 24;
-			client.setResults(count);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
 			let [ err, results ] = await asyncHandler(
 				observationService.getCount(null, mockLogger)
 			);
 
 			expect(err).toBeUndefined();
-			expect(results).toEqual(24);
+			expect(results).toEqual(1);
 		});
 
 	});
 
 	describe('Method: getObservation', () => {
 
-		test('should correctly pass back an error', async () => {
-			// Set our global client to error on it's next call
-			let message = 'Unable to connect to DB';
-			client.setError(message);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
-			let [ err, results ] = await asyncHandler(
-				observationService.getObservation({ query: {}}, mockLogger)
-			);
-
-			expect(results).toBeUndefined();
-			expect(err.message).toEqual(message);
-		});
-
 		test('should correctly return a document', async () => {
 			// Set the results for our global client
-			let observation = { id: 12 };
-			let query = { patient: 2, category: 'foo', code: 'bar', date: new Date().toISOString() };
-			client.setResults(observation);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
-			let [ err, results ] = await asyncHandler(
-				observationService.getObservation({ query }, mockLogger)
+			// let query = { patient: 2, category: 'foo', code: 'bar', date: new Date().toISOString() };
+			let [ err, docs ] = await asyncHandler(
+				observationService.getObservation({ query: {} }, mockLogger)
 			);
 
 			expect(err).toBeUndefined();
-			expect(results.id).toEqual(observation.id);
+			// expect(docs.id).toEqual(observation.id);
 		});
 
 	});
 
 	describe('Method: getObservationByID', () => {
 
-		test('should correctly pass back an error', async () => {
-			// Set our global client to error on it's next call
-			let message = 'Unable to connect to DB';
-			client.setError(message);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
-			let [ err, results ] = await asyncHandler(
-				observationService.getObservationByID({ params: {}}, mockLogger)
-			);
-
-			expect(results).toBeUndefined();
-			expect(err.message).toEqual(message);
-		});
-
 		test('should correctly return a document', async () => {
 			// Set the results for our global client
-			let observation = { id: 12 };
-			client.setResults(observation);
-			// Invoke our service and test it correctly returns the error we gave it
-			// It takes a req and a logger and returns a promise
-			let [ err, results ] = await asyncHandler(
-				observationService.getObservationByID({ params: observation }, mockLogger)
+			let params = { id: 1 };
+			let [ err, docs ] = await asyncHandler(
+				observationService.getObservationByID({ params }, mockLogger)
 			);
 
 			expect(err).toBeUndefined();
-			expect(results.id).toEqual(observation.id);
+			expect(docs.id).toEqual(params.id);
 		});
 
 	});
