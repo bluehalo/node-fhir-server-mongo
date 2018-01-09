1419b859158a8c6fb8bc5fda0af1e568f5a0d2db
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-01-09 10:26:21 -0500

    Updated env variables

--- fhir/src/config.js
+++ fhir/src/config.js
@@ -19,11 +19,11 @@ module.exports.mongoConfig = {
  */
 module.exports.fhirServerConfig = {
 	auth: {
-		clientId: 'nobody_special',
-		secret: 'super_secret',
-		discoveryUrl: 'https://sb-auth.smarthealthit.org/.well-known/openid-configuration',
-		protectedResourceClientId: 'random_unimportant_id',
-		protectedResourceClientSecret: 'random_unimportant_secret'
+		clientId: env.AUTH_CLIENT_ID,
+		secret: env.AUTH_CLIENT_SECRET,
+		discoveryUrl: env.AUTH_DISCOVERY_URL,
+		protectedResourceClientId: env.AUTH_PROTECTED_CLIENT_ID,
+		protectedResourceClientSecret: env.AUTH_PROTECTED_CLIENT_SECRET
 	},
 	server: {
 		port: env.SERVER_PORT,
