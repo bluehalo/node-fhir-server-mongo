88d6ce04885736336774eb4d99f495614751a485
Jon Lee <jlee@asymmetrik.com>
2018-01-12 10:45:25 -0500

    Removing findById since it’s not being used

--- fhir/src/services/oauth/client.service.js
+++ fhir/src/services/oauth/client.service.js
@@ -2,30 +2,7 @@ const { COLLECTION, CLIENT_DB } = require('../../constants');
 const globals = require('../../globals');
 
 /**
- * @name findById
- * @description Get a client by ID from our database
- * @param {Express.req} req - Express request object
- * @param {Winston} logger - Winston logger
- * @return {Promise}
- */
-module.exports.findById = (id, logger) => new Promise((resolve, reject) => {
-	// Grab an instance of our DB and collection
-	let db = globals.get(CLIENT_DB);
-	let collection = db.collection(COLLECTION.CLIENT);
-
-	// Query our collection for this client
-	return collection.findOne({ id }, (err, client) => {
-		if (err) {
-			logger.error('Error with Client.findById: ', err);
-			return reject(err);
-		}
-		resolve(client);
-	});
-});
-
-
-/**
- * @name getObservationById
+ * @name findByClientId
  * @description Get an client by the client ID from our database
  * @param {Express.req} req - Express request object
  * @param {Winston} logger - Winston logger
