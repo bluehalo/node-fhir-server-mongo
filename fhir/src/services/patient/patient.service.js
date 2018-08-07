c4dd72155d96357d869412fedeff409b058c869d
Jon Lee <jlee@asymmetrik.com>
2018-08-07 15:21:08 -0400

    Add history services

--- fhir/src/services/patient/patient.service.js
+++ fhir/src/services/patient/patient.service.js
@@ -247,7 +247,7 @@ module.exports.searchById = (args, logger) => new Promise((resolve, reject) => {
 
 	// Grab an instance of our DB and collection
 	let db = globals.get(CLIENT_DB);
-	let collection = db.collection(COLLECTION.PATIENT);
+	let collection = db.collection(COLLECTION.PATIENT + 'History');
 	// Query our collection for this observation
 	collection.findOne({ id: id.toString() }, (err, patient) => {
 		if (err) {
@@ -303,9 +303,6 @@ module.exports.update = (args, logger) => new Promise((resolve, reject) => {
 	logger.info('Patient >>> update');
 	let { base = 'stu3', id, resource } = args;
 
-	// Grab an instance of our DB and collection
-	let db = globals.get(CLIENT_DB);
-	let collection = db.collection(COLLECTION.PATIENT);
 	// Set the id of the resource
 	if (resource.meta) {
 		// should use auto increment
@@ -317,19 +314,32 @@ module.exports.update = (args, logger) => new Promise((resolve, reject) => {
 	}
 
 	let cleaned = JSON.parse(JSON.stringify(resource));
-
 	let doc = Object.assign(cleaned, { _id: id });
 
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let patientCollection = db.collection(COLLECTION.PATIENT);
+
 	// Insert/update our patient record
-	collection.findOneAndUpdate({ id: id }, { $set: doc}, { upsert: true }, (err, res) => {
+	patientCollection.findOneAndUpdate({ id: id }, { $set: doc }, { upsert: true }, (err, res) => {
 		if (err) {
 			logger.error('Error with Patient.update: ', err);
 			return reject(err);
 		}
 
-		// If we support versioning, which we do not at the moment,
-		// we need to return a version
-		return resolve({ id: res.value && res.value.id, created: res.lastErrorObject && !res.lastErrorObject.updatedExisting, resource_version: doc.meta.versionId });
+		// save to history
+		let historyCollection = db.collection(COLLECTION.PATIENT + 'History');
+
+		// Insert our patient record to history but don't assign _id
+		return historyCollection.insert(cleaned, (err2) => {
+			if (err2) {
+				logger.error('Error with PatientHistory.create: ', err2);
+				return reject(err2);
+			}
+
+			return resolve({ id: res.value && res.value.id, created: res.lastErrorObject && !res.lastErrorObject.updatedExisting, resource_version: doc.meta.versionId });
+		});
+
 	});
 });
 
@@ -360,5 +370,391 @@ module.exports.remove = (args, logger) => new Promise((resolve, reject) => {
 			});
 		}
 		return resolve();
+	});
+});
+
+module.exports.searchByVersionId = (args, context, logger) => new Promise((resolve, reject) => {
+
+	logger.info('Patient >>> searchByVersionId');
+
+	console.log(args);
+	// Parse the required params, these are validated by sanitizeMiddleware in core
+	let { id, base, version_id } = args;
+
+	let Patient = getPatient(base);
+
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(COLLECTION.PATIENT + 'History');
+	// Query our collection for this observation
+	collection.findOne({ id: id.toString(), 'meta.versionId': `${version_id}` }, (err, patient) => {
+		if (err) {
+			logger.error('Error with Patient.searchById: ', err);
+			return reject(err);
+		}
+
+		if (patient) {
+			resolve(new Patient(patient));
+		}
+
+		resolve();
+
+	});
+});
+
+
+module.exports.historyById = (args, logger) => new Promise((resolve, reject) => {
+
+	logger.info('Patient >>> historyById');
+
+	// Parse the params
+	let { base, _id, active, address, addressCity, addressCountry, addressPostalCode, addressState, addressUse, animalBreed,
+		animalSpecies, birthDate, deathDate, deceased, email, family, gender, generalPractitioner, given, identifier,
+		language, link, name, organization, phone, /*phonetic,*/ telecom } = args;
+	let query = {};
+	let ors = [];
+
+	// Handle all arguments that have or logic
+	if (address) {
+		let orsAddress = addressQueryBuilder(address);
+		for (let i = 0; i < orsAddress.length; i++) {
+			ors.push(orsAddress[i]);
+		}
+	}
+	if (name) {
+		let orsName = nameQueryBuilder(name);
+		for (let i = 0; i < orsName.length; i++) {
+			ors.push(orsName[i]);
+		}
+	}
+	if (ors.length !== 0) {
+		query.$and = ors;
+	}
+
+	if (_id) {
+		query.id = _id;
+	}
+
+	if (active) {
+		query.active = (active === 'true');
+	}
+
+	if (addressCity) {
+		query['address.city'] = stringQueryBuilder(addressCity);
+	}
+
+	if (addressCountry) {
+		query['address.country'] = stringQueryBuilder(addressCountry);
+	}
+
+	if (addressPostalCode) {
+		query['address.postalCode'] = stringQueryBuilder(addressPostalCode);
+	}
+
+	if (addressState) {
+		query['address.state'] = stringQueryBuilder(addressState);
+	}
+
+	if (addressUse) {
+		query['address.use'] = addressUse;
+	}
+
+	if (animalBreed) {
+		let queryBuilder = tokenQueryBuilder(animalBreed, 'code', 'animal.breed.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (animalSpecies) {
+		let queryBuilder = tokenQueryBuilder(animalSpecies, 'code', 'animal.species.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (birthDate) {
+		query.birthDate = dateQueryBuilder(birthDate, 'date', '');
+	}
+
+	if (deathDate) {
+		query.deceasedDateTime = dateQueryBuilder(deathDate, 'dateTime', '');
+	}
+
+	if (deceased) {
+		query.deceasedBoolean = (deceased === 'true');
+	}
+
+	// Forces system = 'email'
+	if (email) {
+		let queryBuilder = tokenQueryBuilder(email, 'value', 'telecom', 'email');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (family) {
+		query['name.family'] = stringQueryBuilder(family);
+	}
+
+	if (gender) {
+		query.gender = gender;
+	}
+
+	if (generalPractitioner) {
+		let queryBuilder = referenceQueryBuilder(generalPractitioner, 'generalPractitioner.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (given) {
+		query['name.given'] = stringQueryBuilder(given);
+	}
+
+	if (identifier) {
+		let queryBuilder = tokenQueryBuilder(identifier, 'value', 'identifier', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (language) {
+		let queryBuilder = tokenQueryBuilder(language, 'code', 'communication.language.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (link) {
+		let queryBuilder = referenceQueryBuilder(link, 'link.other.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (organization) {
+		let queryBuilder = referenceQueryBuilder(organization, 'managingOrganization.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// Forces system = 'phone'
+	if (phone) {
+		let queryBuilder = tokenQueryBuilder(phone, 'value', 'telecom', 'phone');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// if (phonetic) {
+	//
+	// }
+
+	if (telecom) {
+		let queryBuilder = tokenQueryBuilder(telecom, 'value', 'telecom', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(COLLECTION.PATIENT + 'History');
+	let Patient = getPatient(base);
+
+	// Query our collection for this observation
+	collection.find(query, (err, data) => {
+		if (err) {
+			logger.error('Error with Patient.search: ', err);
+			return reject(err);
+		}
+
+		// Patient is a patient cursor, pull documents out before resolving
+		data.toArray().then((patients) => {
+			patients.forEach(function(element, i, returnArray) {
+				returnArray[i] = new Patient(element);
+			});
+			resolve(patients);
+		});
+	});
+});
+
+module.exports.history = (args, logger) => new Promise((resolve, reject) => {
+
+	logger.info('Patient >>> history');
+
+	// Parse the params
+	let { base, _id, active, address, addressCity, addressCountry, addressPostalCode, addressState, addressUse, animalBreed,
+		animalSpecies, birthDate, deathDate, deceased, email, family, gender, generalPractitioner, given, identifier,
+		language, link, name, organization, phone, /*phonetic,*/ telecom } = args;
+	let query = {};
+	let ors = [];
+
+	// Handle all arguments that have or logic
+	if (address) {
+		let orsAddress = addressQueryBuilder(address);
+		for (let i = 0; i < orsAddress.length; i++) {
+			ors.push(orsAddress[i]);
+		}
+	}
+	if (name) {
+		let orsName = nameQueryBuilder(name);
+		for (let i = 0; i < orsName.length; i++) {
+			ors.push(orsName[i]);
+		}
+	}
+	if (ors.length !== 0) {
+		query.$and = ors;
+	}
+
+	if (_id) {
+		query.id = _id;
+	}
+
+	if (active) {
+		query.active = (active === 'true');
+	}
+
+	if (addressCity) {
+		query['address.city'] = stringQueryBuilder(addressCity);
+	}
+
+	if (addressCountry) {
+		query['address.country'] = stringQueryBuilder(addressCountry);
+	}
+
+	if (addressPostalCode) {
+		query['address.postalCode'] = stringQueryBuilder(addressPostalCode);
+	}
+
+	if (addressState) {
+		query['address.state'] = stringQueryBuilder(addressState);
+	}
+
+	if (addressUse) {
+		query['address.use'] = addressUse;
+	}
+
+	if (animalBreed) {
+		let queryBuilder = tokenQueryBuilder(animalBreed, 'code', 'animal.breed.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (animalSpecies) {
+		let queryBuilder = tokenQueryBuilder(animalSpecies, 'code', 'animal.species.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (birthDate) {
+		query.birthDate = dateQueryBuilder(birthDate, 'date', '');
+	}
+
+	if (deathDate) {
+		query.deceasedDateTime = dateQueryBuilder(deathDate, 'dateTime', '');
+	}
+
+	if (deceased) {
+		query.deceasedBoolean = (deceased === 'true');
+	}
+
+	// Forces system = 'email'
+	if (email) {
+		let queryBuilder = tokenQueryBuilder(email, 'value', 'telecom', 'email');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (family) {
+		query['name.family'] = stringQueryBuilder(family);
+	}
+
+	if (gender) {
+		query.gender = gender;
+	}
+
+	if (generalPractitioner) {
+		let queryBuilder = referenceQueryBuilder(generalPractitioner, 'generalPractitioner.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (given) {
+		query['name.given'] = stringQueryBuilder(given);
+	}
+
+	if (identifier) {
+		let queryBuilder = tokenQueryBuilder(identifier, 'value', 'identifier', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (language) {
+		let queryBuilder = tokenQueryBuilder(language, 'code', 'communication.language.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (link) {
+		let queryBuilder = referenceQueryBuilder(link, 'link.other.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (organization) {
+		let queryBuilder = referenceQueryBuilder(organization, 'managingOrganization.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// Forces system = 'phone'
+	if (phone) {
+		let queryBuilder = tokenQueryBuilder(phone, 'value', 'telecom', 'phone');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// if (phonetic) {
+	//
+	// }
+
+	if (telecom) {
+		let queryBuilder = tokenQueryBuilder(telecom, 'value', 'telecom', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(COLLECTION.PATIENT + 'History');
+	let Patient = getPatient(base);
+
+	// Query our collection for this observation
+	collection.find(query, (err, data) => {
+		if (err) {
+			logger.error('Error with Patient.search: ', err);
+			return reject(err);
+		}
+
+		// Patient is a patient cursor, pull documents out before resolving
+		data.toArray().then((patients) => {
+			patients.forEach(function(element, i, returnArray) {
+				returnArray[i] = new Patient(element);
+			});
+			resolve(patients);
+		});
 	});
 });
