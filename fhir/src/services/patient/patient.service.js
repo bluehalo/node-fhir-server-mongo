40b8f8c57248af70aa34b2157481332b24b92f0c
Jon Lee <jlee@asymmetrik.com>
2018-08-07 20:55:00 -0400

    Return n - number of documents deleted

--- fhir/src/services/patient/patient.service.js
+++ fhir/src/services/patient/patient.service.js
@@ -399,7 +399,7 @@ module.exports.remove = (args, logger) => new Promise((resolve, reject) => {
 				});
 			}
 
-			return resolve({ deleted: _.result.ok });
+			return resolve({ deleted: _.result && _.result.n });
 		});
 
 	});
