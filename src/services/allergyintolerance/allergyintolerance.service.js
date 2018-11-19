877d7455e5cd517d1dbe2115871255614d6c31a6
Frazer Smith <frazer.smith@ydh.nhs.uk>
2018-11-19 14:50:10 +0000

    Update allergyintolerance service

--- src/services/allergyintolerance/allergyintolerance.service.js
+++ src/services/allergyintolerance/allergyintolerance.service.js
@@ -1,8 +1,16 @@
 /*eslint no-unused-vars: "warn"*/
 
-const { RESOURCES } = require('@asymmetrik/node-fhir-server-core').constants;
+const { RESOURCES, VERSIONS } = require('@asymmetrik/node-fhir-server-core').constants;
 const FHIRServer = require('@asymmetrik/node-fhir-server-core');
 const { ObjectID } = require('mongodb');
+const { COLLECTION, CLIENT_DB } = require('../../constants');
+const moment = require('moment-timezone');
+const globals = require('../../globals');
+
+const { dateQueryBuilder,
+	tokenQueryBuilder,
+	referenceQueryBuilder } = require('../../utils/querybuilder.util');
+
 
 let getAllergyIntolerance = (base_version) => {
 	return require(FHIRServer.resolveFromVersion(base_version, RESOURCES.ALLERGYINTOLERANCE));};
@@ -10,16 +18,16 @@ let getAllergyIntolerance = (base_version) => {
 let getMeta = (base_version) => {
 	return require(FHIRServer.resolveFromVersion(base_version, RESOURCES.META));};
 
-module.exports.search = (args, context, logger) => new Promise((resolve, reject) => {
-	logger.info('AllergyIntolerance >>> search');
+
+let buildStu3SearchQuery = (args) =>	 {
 
 	// Common search params
-	let { base_version, _content, _format, _id, _lastUpdated, _profile, _query, _security, _tag } = args;
+	let { _content, _format, _id, _lastUpdated, _profile, _query, _security, _tag } = args;
 
 	// Search Result params
 	let { _INCLUDE, _REVINCLUDE, _SORT, _COUNT, _SUMMARY, _ELEMENTS, _CONTAINED, _CONTAINEDTYPED } = args;
 
-	// Resource Specific params
+	// AllergyIntolerance search params
 	let asserter = args['asserter'];
 	let category = args['category'];
 	let clinical_status = args['clinical-status'];
@@ -37,40 +45,161 @@ module.exports.search = (args, context, logger) => new Promise((resolve, reject)
 	let type = args['type'];
 	let verification_status = args['verification-status'];
 
-	// TODO: Build query from Parameters
+	let query = {};
 
-	// TODO: Query database
+	if (asserter) {
+		let queryBuilder = referenceQueryBuilder(asserter, 'asserter.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
 
+	if (category) {
+		query.category = category;
+	}
+
+	if (clinical_status) {
+		query.clinicalStatus = clinical_status;
+	}
+
+	if (code) {
+		query.$or = [tokenQueryBuilder(code, 'code', 'code.coding'), tokenQueryBuilder(code, 'code', 'reaction.substance.coding')];
+	}
+
+	if (criticality) {
+		query.criticality = criticality;
+	}
+
+	if (date) {
+		query['assertedDate'] = dateQueryBuilder(date);
+	}
+
+	if (identifier) {
+		let queryBuilder = tokenQueryBuilder(identifier, 'value', 'identifier', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}	
+	}
+
+	if (last_date) {
+		query['lastOccurrence'] = dateQueryBuilder(last_date);
+	}
+
+	if (manifestation) {
+		let queryBuilder = tokenQueryBuilder(manifestation, 'code', 'reaction.manifestation.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (onset) {
+		query['reaction.onset'] = dateQueryBuilder(onset);
+	}
+
+	if (patient) {
+		let queryBuilder = referenceQueryBuilder(patient, 'patient.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (recorder) {
+		let queryBuilder = referenceQueryBuilder(recorder, 'recorder.reference');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (route) {
+		let queryBuilder = tokenQueryBuilder(route, 'code', 'reaction.exposureRoute.coding', '');
+		for (let i in queryBuilder) {
+			query[i] = queryBuilder[i];
+		}
+	}
+
+	if (severity) {
+		query['reaction.severity'] = severity;
+	}
+
+	if (type) {
+		query.type = type;
+	}
+
+	if (verification_status) {
+		query['verificationStatus'] = verification_status;
+	}
+
+	return query;
+
+};
+
+let buildDstu2SearchQuery = (args) =>	 {
+
+	let query = {};
+
+	return query;
+};
+
+/**
+ *
+ * @param {*} args
+ * @param {*} context
+ * @param {*} logger
+ */
+module.exports.search = (args, context, logger) => new Promise((resolve, reject) => {
+	logger.info('AllergyIntolerance >>> search');
+
+	let { base_version } = args;
+	let query = {};
+
+	if (base_version === VERSIONS['3_0_1']) {
+		query = buildStu3SearchQuery(args);
+	} else if (base_version === VERSIONS['1_0_2']) {
+		query = buildDstu2SearchQuery(args);
+	}
+
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}`);
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
 
-	// Cast all results to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance();
-	// TODO: Set data with constructor or setter methods
-	allergyintolerance_resource.id = 'test id';
+	// Query our collection for this observation
+	collection.find(query, (err, data) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.search: ', err);
+			return reject(err);
+		}
 
-	// Return Array
-	resolve([allergyintolerance_resource]);
+		// AllergyIntolerance is a allergy_intolerance cursor, pull documents out before resolving
+		data.toArray().then((allergyintolerance_resources) => {
+			allergyintolerance_resources.forEach(function(element, i, returnArray) {
+				returnArray[i] = new AllergyIntolerance(element);
+			});
+			resolve(allergyintolerance_resources);
+		});
+	});
 });
 
 module.exports.searchById = (args, context, logger) => new Promise((resolve, reject) => {
 	logger.info('AllergyIntolerance >>> searchById');
 
 	let { base_version, id } = args;
-
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
 
-	// TODO: Build query from Parameters
-
-	// TODO: Query database
-
-	// Cast result to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance();
-	// TODO: Set data with constructor or setter methods
-	allergyintolerance_resource.id = 'test id';
-
-	// Return resource class
-	// resolve(allergyintolerance_resource);
-	resolve();
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}`);
+	// Query our collection for this observation
+	collection.findOne({ id: id.toString() }, (err, allergyintolerance_resource) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.searchById: ', err);
+			return reject(err);
+		}
+		if (allergyintolerance_resource) {
+			resolve(new AllergyIntolerance(allergyintolerance_resource));
+		}
+		resolve();
+	});
 });
 
 module.exports.create = (args, context, logger) => new Promise((resolve, reject) => {
@@ -80,20 +209,44 @@ module.exports.create = (args, context, logger) => new Promise((resolve, reject)
 	// Make sure to use this ID when inserting this resource
 	let id = new ObjectID().toString();
 
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}`);
+
+	// get current record
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
+	let allergyintolerance = new AllergyIntolerance(resource);
+
 	let Meta = getMeta(base_version);
+	allergyintolerance.meta = new Meta({versionId: '1', lastUpdated: moment.utc().format('YYYY-MM-DDTHH:mm:ssZ')});
 
-	// TODO: determine if client/server sets ID
+	let cleaned = JSON.parse(JSON.stringify(allergyintolerance.toJSON()));
+	let doc = Object.assign(cleaned, { _id: id });
 
-	// Cast resource to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance(resource);
-	allergyintolerance_resource.meta = new Meta();
-	// TODO: set meta info
+	// Insert/update our allergyintolerance record
+	collection.updateOne({ id: id }, { $set: doc }, { upsert: true }, (err2, res) => {
+		if (err2) {
+			logger.error('Error with AllergyIntolerance.create: ', err2);
+			return reject(err2);
+		}
 
-	// TODO: save record to database
+		// save to history
+		let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
 
+		let history_allergyintolerance = Object.assign(cleaned, { id: id });
+
+		// Insert our allergyintolerance record to history but don't assign _id
+		return history_collection.insertOne(history_allergyintolerance, (err3) => {
+			if (err3) {
+				logger.error('Error with AllergyIntoleranceHistory.create: ', err3);
+				return reject(err3);
+			}
+
+			return resolve({ id: id, created: res.lastErrorObject && !res.lastErrorObject.updatedExisting, resource_version: doc.meta.versionId });
+		});
+
+	});
 	// Return Id
-	resolve({ id });
 });
 
 module.exports.update = (args, context, logger) => new Promise((resolve, reject) => {
@@ -101,29 +254,101 @@ module.exports.update = (args, context, logger) => new Promise((resolve, reject)
 
 	let { base_version, id, resource } = args;
 
-	let AllergyIntolerance = getAllergyIntolerance(base_version);
-	let Meta = getMeta(base_version);
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}`);
 
-	// Cast resource to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance(resource);
-	allergyintolerance_resource.meta = new Meta();
-	// TODO: set meta info, increment meta ID
+	// get current record
+	// Query our collection for this observation
+	collection.findOne({ id: id.toString() }, (err, data) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.searchById: ', err);
+			return reject(err);
+		}
 
-	// TODO: save record to database
+		let AllergyIntolerance = getAllergyIntolerance(base_version);
+		let allergyintolerance = new AllergyIntolerance(resource);
 
-	// Return id, if recorded was created or updated, new meta version id
-	resolve({ id: allergyintolerance_resource.id, created: false, resource_version: allergyintolerance_resource.meta.versionId });
+		if (data && data.meta) {
+			let foundAllergyIntolerance = new AllergyIntolerance(data);
+			let meta = foundAllergyIntolerance.meta;
+			meta.versionId = `${parseInt(foundAllergyIntolerance.meta.versionId) + 1}`;
+			allergyintolerance.meta = meta;
+		} else {
+			let Meta = getMeta(base_version);
+			allergyintolerance.meta = new Meta({versionId: '1', lastUpdated: moment.utc().format('YYYY-MM-DDTHH:mm:ssZ')});
+		}
+
+		let cleaned = JSON.parse(JSON.stringify(allergyintolerance_resource));
+		let doc = Object.assign(cleaned, { _id: id });
+
+		// Insert/update our allergyintolerance record
+		collection.findOneAndUpdate({ id: id }, { $set: doc }, { upsert: true }, (err2, res) => {
+			if (err2) {
+				logger.error('Error with AllergyIntolerance.update: ', err2);
+				return reject(err2);
+			}
+
+			// save to history
+			let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
+
+			let history_allergyintolerance = Object.assign(cleaned, { id: id });
+
+			// Insert our allergyintolerance record to history but don't assign _id
+			return history_collection.insertOne(history_allergyintolerance, (err3) => {
+				if (err3) {
+					logger.error('Error with AllergyIntoleranceHistory.create: ', err3);
+					return reject(err3);
+				}
+
+				return resolve({ id: id, created: res.lastErrorObject && !res.lastErrorObject.updatedExisting, resource_version: doc.meta.versionId });
+			});
+
+		});
+	});
 });
 
 module.exports.remove = (args, context, logger) => new Promise((resolve, reject) => {
 	logger.info('AllergyIntolerance >>> remove');
 
-	let { id } = args;
+	let { base_version, id } = args;
 
-	// TODO: delete record in database (soft/hard)
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}`);
+	// Delete our allergyintolerance record
+	collection.deleteOne({ id: id }, (err, _) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.remove');
+			return reject({
+				// Must be 405 (Method Not Allowed) or 409 (Conflict)
+				// 405 if you do not want to allow the delete
+				// 409 if you can't delete because of referential
+				// integrity or some other reason
+				code: 409,
+				message: err.message
+			});
+		}
 
-	// Return number of records deleted
-	resolve({ deleted: 0 });
+		// delete history as well.  You can chose to save history.  Up to you
+		let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
+		return history_collection.deleteMany({ id: id }, (err2) => {
+			if (err2) {
+				logger.error('Error with AllergyIntolerance.remove');
+				return reject({
+					// Must be 405 (Method Not Allowed) or 409 (Conflict)
+					// 405 if you do not want to allow the delete
+					// 409 if you can't delete because of referential
+					// integrity or some other reason
+					code: 409,
+					message: err2.message
+				});
+			}
+
+			return resolve({ deleted: _.result && _.result.n });
+		});
+
+	});
 });
 
 module.exports.searchByVersionId = (args, context, logger) => new Promise((resolve, reject) => {
@@ -133,93 +358,93 @@ module.exports.searchByVersionId = (args, context, logger) => new Promise((resol
 
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
 
-	// TODO: Build query from Parameters
+	let db = globals.get(CLIENT_DB);
+	let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
 
-	// TODO: Query database
+	// Query our collection for this observation
+	history_collection.findOne({ id: id.toString(), 'meta.versionId': `${version_id}` }, (err, allergyintolerance_resource) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.searchByVersionId: ', err);
+			return reject(err);
+		}
 
-	// Cast result to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance();
+		if (allergyintolerance_resource) {
+			resolve(new AllergyIntolerance(allergyintolerance_resource));
+		}
 
-	// Return resource class
-	resolve(allergyintolerance_resource);
+		resolve();
+
+	});
 });
 
 module.exports.history = (args, context, logger) => new Promise((resolve, reject) => {
 	logger.info('AllergyIntolerance >>> history');
 
 	// Common search params
-	let { base_version, _content, _format, _id, _lastUpdated, _profile, _query, _security, _tag } = args;
+	let { base_version } = args;
 
-	// Search Result params
-	let { _INCLUDE, _REVINCLUDE, _SORT, _COUNT, _SUMMARY, _ELEMENTS, _CONTAINED, _CONTAINEDTYPED } = args;
+	let query = {};
 
-	// Resource Specific params
-	let asserter = args['asserter'];
-	let category = args['category'];
-	let clinical_status = args['clinical-status'];
-	let code = args['code'];
-	let criticality = args['criticality'];
-	let date = args['date'];
-	let identifier = args['identifier'];
-	let last_date = args['last-date'];
-	let manifestation = args['manifestation'];
-	let onset = args['onset'];
-	let patient = args['patient'];
-	let recorder = args['recorder'];
-	let route = args['route'];
-	let severity = args['severity'];
-	let type = args['type'];
-	let verification_status = args['verification-status'];
+	if (base_version === VERSIONS['3_0_1']) {
+		query = buildStu3SearchQuery(args);
+	} else if (base_version === VERSIONS['1_0_2']) {
+		query = buildDstu2SearchQuery(args);
+	}
 
-	// TODO: Build query from Parameters
-
-	// TODO: Query database
-
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
 
-	// Cast all results to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance();
+	// Query our collection for this observation
+	history_collection.find(query, (err, data) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.history: ', err);
+			return reject(err);
+		}
 
-	// Return Array
-	resolve([allergyintolerance_resource]);
+		// AllergyIntolerance is a allergyintolerance cursor, pull documents out before resolving
+		data.toArray().then((allergyintolerance_resources) => {
+			allergyintolerance_resources.forEach(function(element, i, returnArray) {
+				returnArray[i] = new AllergyIntolerance(element);
+			});
+			resolve(allergyintolerance_resources);
+		});
+	});
 });
 
 module.exports.historyById = (args, context, logger) => new Promise((resolve, reject) => {
 	logger.info('AllergyIntolerance >>> historyById');
 
-	// Common search params
-	let { base_version, _content, _format, _id, _lastUpdated, _profile, _query, _security, _tag } = args;
+	let { base_version, id } = args;
+	let query = {};
 
-	// Search Result params
-	let { _INCLUDE, _REVINCLUDE, _SORT, _COUNT, _SUMMARY, _ELEMENTS, _CONTAINED, _CONTAINEDTYPED } = args;
+	if (base_version === VERSIONS['3_0_1']) {
+		query = buildStu3SearchQuery(args);
+	} else if (base_version === VERSIONS['1_0_2']) {
+		query = buildDstu2SearchQuery(args);
+	}
 
-	// Resource Specific params
-	let asserter = args['asserter'];
-	let category = args['category'];
-	let clinical_status = args['clinical-status'];
-	let code = args['code'];
-	let criticality = args['criticality'];
-	let date = args['date'];
-	let identifier = args['identifier'];
-	let last_date = args['last-date'];
-	let manifestation = args['manifestation'];
-	let onset = args['onset'];
-	let patient = args['patient'];
-	let recorder = args['recorder'];
-	let route = args['route'];
-	let severity = args['severity'];
-	let type = args['type'];
-	let verification_status = args['verification-status'];
+	query.id = `${id}`;
 
-	// TODO: Build query from Parameters
-
-	// TODO: Query database
-
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let history_collection = db.collection(`${COLLECTION.ALLERGYINTOLERANCE}_${base_version}_History`);
 	let AllergyIntolerance = getAllergyIntolerance(base_version);
 
-	// Cast all results to AllergyIntolerance Class
-	let allergyintolerance_resource = new AllergyIntolerance();
+	// Query our collection for this observation
+	history_collection.find(query, (err, data) => {
+		if (err) {
+			logger.error('Error with AllergyIntolerance.historyById: ', err);
+			return reject(err);
+		}
 
-	// Return Array
-	resolve([allergyintolerance_resource]);
+		// AllergyIntolerance is a allergyintolerance cursor, pull documents out before resolving
+		data.toArray().then((allergyintolerance_resources) => {
+			allergyintolerance_resources.forEach(function(element, i, returnArray) {
+				returnArray[i] = new AllergyIntolerance(element);
+			});
+			resolve(allergyintolerance_resources);
+		});
+	});
 });
