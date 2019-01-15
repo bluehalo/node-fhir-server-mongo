3370f8d7c30d08b670fa92e9ab143ceee8b9bcef
Sumeet Shah <sshah@asymmetrik.com>
2019-01-15 13:20:06 -0500

    added support for JSON patch

--- src/services/patient/patient.service.js
+++ src/services/patient/patient.service.js
@@ -5,6 +5,7 @@ const FHIRServer = require('@asymmetrik/node-fhir-server-core');
 const { COLLECTION, CLIENT_DB } = require('../../constants');
 const moment = require('moment-timezone');
 const globals = require('../../globals');
+const jsonpatch = require('fast-json-patch');
 
 const { getUuid } = require('../../utils/uid.util');
 
@@ -496,22 +497,22 @@ module.exports.create = (args, context, logger) => new Promise((resolve, reject)
 
 	// Insert our patient record
 	collection.insertOne(doc, (err) => {
-			if (err) {
-				logger.error('Error with Patient.create: ', err);
-				return reject(err);
+		if (err) {
+			logger.error('Error with Patient.create: ', err);
+			return reject(err);
+		}
+
+		// Save the resource to history
+		let history_collection = db.collection(`${COLLECTION.PATIENT}_${base_version}_History`);
+
+		// Insert our patient record to history but don't assign _id
+		return history_collection.insertOne(history_doc, (err2) => {
+			if (err2) {
+				logger.error('Error with PatientHistory.create: ', err2);
+				return reject(err2);
 			}
-
-			// Save the resource to history
-			let history_collection = db.collection(`${COLLECTION.PATIENT}_${base_version}_History`);
-
-			// Insert our patient record to history but don't assign _id
-			return history_collection.insertOne(history_doc, (err2) => {
-				if (err2) {
-					logger.error('Error with PatientHistory.create: ', err2);
-					return reject(err2);
-				}
-				return resolve({ id: doc.id, resource_version: doc.meta.versionId });
-			});
+			return resolve({ id: doc.id, resource_version: doc.meta.versionId });
+		});
 	});
 	// Return Id - TODO ss should I be returning ID?
 });
@@ -527,7 +528,7 @@ module.exports.update = (args, context, logger) => new Promise((resolve, reject)
 	let db = globals.get(CLIENT_DB);
 	let collection = db.collection(`${COLLECTION.PATIENT}_${base_version}`);
 
-	// get current record
+	// Get current record
 	// Query our collection for this observation
 	collection.findOne({ id: id.toString() }, (err, data) => {
 		if (err) {
@@ -713,6 +714,72 @@ module.exports.historyById = (args, context, logger) => new Promise((resolve, re
 				returnArray[i] = new Patient(element);
 			});
 			resolve(patients);
+		});
+	});
+});
+
+module.exports.patch = (args, context, logger) => new Promise((resolve, reject) => {
+	logger.info('Patient >>> patch'); // Should this say update (instead of patch) because the end result is that of an update, not a patch
+
+	let { base_version, id, patchContent } = args;
+
+	// Grab an instance of our DB and collection
+	let db = globals.get(CLIENT_DB);
+	let collection = db.collection(`${COLLECTION.PATIENT}_${base_version}`);
+
+	// Get current record
+	// Query our collection for this observation
+	collection.findOne({ id: id.toString() }, (err, data) => {
+		if (err) {
+			logger.error('Error with Patient.searchById: ', err);
+			return reject(err);
+		}
+
+		// Validate the patch
+		let errors = jsonpatch.validate(patchContent, data);
+		if (errors && Object.keys(errors).length > 0) {
+			logger.error('Error with patch contents');
+			return reject(errors);
+		}
+		// Make the changes indicated in the patch
+		let resource = jsonpatch.applyPatch(data, patchContent).newDocument;
+
+		let Patient = getPatient(base_version);
+		let patient = new Patient(resource);
+
+		if (data && data.meta) {
+			let foundPatient = new Patient(data);
+			let meta = foundPatient.meta;
+			meta.versionId = `${parseInt(foundPatient.meta.versionId) + 1}`;
+			patient.meta = meta;
+		} else {
+			return reject('Unable to patch resource. Missing either data or metadata.');
+		}
+
+		// Same as update from this point on
+		let cleaned = JSON.parse(JSON.stringify(patient));
+		let doc = Object.assign(cleaned, { _id: id });
+
+		// Insert/update our patient record
+		collection.findOneAndUpdate({ id: id }, { $set: doc }, { upsert: true }, (err2, res) => {
+			if (err2) {
+				logger.error('Error with Patient.update: ', err2);
+				return reject(err2);
+			}
+
+			// Save to history
+			let history_collection = db.collection(`${COLLECTION.PATIENT}_${base_version}_History`);
+			let history_patient = Object.assign(cleaned, { _id: id + cleaned.meta.versionId });
+
+			// Insert our patient record to history but don't assign _id
+			return history_collection.insertOne(history_patient, (err3) => {
+				if (err3) {
+					logger.error('Error with PatientHistory.create: ', err3);
+					return reject(err3);
+				}
+
+				return resolve({ id: doc.id, created: res.lastErrorObject && !res.lastErrorObject.updatedExisting, resource_version: doc.meta.versionId });
+			});
 		});
 	});
 });
