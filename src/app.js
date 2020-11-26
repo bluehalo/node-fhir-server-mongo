4afeaa0c21a7d61b92df4808674593b0f1921f1d
Imran Qureshi <imranq2@hotmail.com>
2020-11-25 20:49:00 -0800

    add /stats (#43)

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get('/clean', async (req, res) => {
         });
 
         console.info('Collection_names:' + collection_names);
-        for (const collection_index in collection_names){
+        for (const collection_index in collection_names) {
             const collection_name = collection_names[collection_index];
             console.log(collection_name);
             console.log(['Removing: ', await db.collection(collection_name).countDocuments({}), ' documents from ', collection_name].join(''));
@@ -43,6 +43,46 @@ app.get('/clean', async (req, res) => {
         }
         await client.close();
         res.status(200).json({ success: true, collections: collection_names });
+    }
+});
+
+app.get('/stats', async (req, res) => {
+    console.info('Running stats');
+
+    // Connect to mongo and pass any options here
+    let [mongoError, client] = await asyncHandler(
+        mongoClient(mongoConfig.connection, mongoConfig.options)
+    );
+    if (mongoError) {
+        console.error(mongoError.message);
+        console.error(mongoConfig.connection);
+        client.close();
+        res.status(500).json({ success: false, error: mongoError });
+    }
+    else {
+        //create client by providing database name
+        const db = client.db(mongoConfig.db_name);
+        var collection_names = [];
+        // const collections = await db.listCollections().toArray();
+
+        await db.listCollections().forEach(async collection => {
+            console.log(collection.name);
+            if (collection.name.indexOf('system.') === -1) {
+                collection_names.push(collection.name);
+            }
+        });
+
+        var collection_stats = [];
+        console.info('Collection_names:' + collection_names);
+        for (const collection_index in collection_names) {
+            const collection_name = collection_names[collection_index];
+            console.log(collection_name);
+            const count = await db.collection(collection_name).countDocuments({});
+            console.log(['Found: ', count, ' documents in ', collection_name].join(''));
+            collection_stats.push({ name: collection_name, count: count });
+        }
+        await client.close();
+        res.status(200).json({ success: true, collections: collection_stats });
     }
 });
 
