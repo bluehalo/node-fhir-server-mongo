ca595a0ece50807b2f4e1ed25f4959bf6904b853
Imran Qureshi <imranq2@hotmail.com>
2021-09-01 23:05:47 -0700

    fix truncation of index name (#249)
    * separate indexing code into its own file
    
    * run indexer in separate process
    
    * run indexer in separate process
    
    * return current indexes
    
    * don't log error if process exit code is 0
    
    * if /show is passed then don't index but just show indexes
    
    * if /index/run is passed then run index creation
    
    * if /index/run is passed then run index creation
    
    * log indexing message to slack
    
    * log indexing message to slack
    
    * log indexing message to slack
    
    * log indexing message to slack
    
    * log indexing message to slack
    
    * make custom index easier to specify
    
    * make custom index easier to specify
    
    * throw error
    
    * set limit on index name
    
    * set limit on index name
    
    * catch errors in indexing, log and keep going
    
    * formatting
    
    * log every index created to slack
    
    * use async await
    
    * use async await
    
    * use async await
    
    * fix truncation of index name

--- src/utils/index.util.js
+++ src/utils/index.util.js
@@ -23,7 +23,12 @@ const customIndexes = {
  * @return {Promise<boolean>}
  */
 async function create_index_if_not_exists(db, property_to_index, collection_name) {
-    const index_name = (property_to_index + '_1').slice(0, env.MAX_INDEX_NAME_LENGTH ? parseInt(env.MAX_INDEX_NAME_LENGTH) - 1 : 124); // max index name length is 125 in mongo
+    // from https://docs.aws.amazon.com/documentdb/latest/developerguide/limits.html#limits.naming
+    // Index name: <col>$<index> :	 Length is [3–63] characters.
+    // total length combines both collection name and index name
+    const mex_index_name_length = (env.MAX_INDEX_NAME_LENGTH ? parseInt(env.MAX_INDEX_NAME_LENGTH) : 63) - collection_name.length - 1;
+
+    const index_name = (property_to_index + '_1').slice(0, mex_index_name_length - 1); // max index name length is 125 in mongo
     try {
         // https://www.tutorialspoint.com/mongodb/mongodb_indexing_limitations.htm#:~:text=A%20collection%20cannot%20have%20more,have%20maximum%2031%20fields%20indexed.
         if (!await db.collection(collection_name).indexExists(index_name)) {
@@ -49,7 +54,12 @@ async function create_index_if_not_exists(db, property_to_index, collection_name
  * @return {Promise<boolean>}
  */
 async function create_multikey_index_if_not_exists(db, properties_to_index, collection_name) {
-    const index_name = (properties_to_index.join('_1_') + '_1').slice(0, env.MAX_INDEX_NAME_LENGTH ? parseInt(env.MAX_INDEX_NAME_LENGTH) - 1 : 124); // max index name length is 125 in mongo
+    // from https://docs.aws.amazon.com/documentdb/latest/developerguide/limits.html#limits.naming
+    // Index name: <col>$<index> :	 Length is [3–63] characters.
+    // total length combines both collection name and index name
+    const mex_index_name_length = (env.MAX_INDEX_NAME_LENGTH ? parseInt(env.MAX_INDEX_NAME_LENGTH) : 63) - collection_name.length - 1;
+
+    const index_name = (properties_to_index.join('_1_') + '_1').slice(0, mex_index_name_length - 1);
     try {
         if (!await db.collection(collection_name).indexExists(index_name)) {
             console.log('Creating multi key index ' + index_name + ' in ' + collection_name);
