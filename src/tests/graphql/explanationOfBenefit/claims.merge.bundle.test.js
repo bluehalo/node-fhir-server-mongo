7ff3e7760b91b877814765bbfe2cbc86d901ab9e
imranq2 <imranq2@hotmail.com>
2021-09-24 09:42:25 -0700

    update graphql tests

--- src/tests/graphql/explanationOfBenefit/claims.merge.bundle.test.js
+++ src/tests/graphql/explanationOfBenefit/claims.merge.bundle.test.js
@@ -22,55 +22,52 @@ describe('GraphQL ExplanationOfBenefit Tests', () => {
         test('GraphQL ExplanationOfBenefit properly', async () => {
             // noinspection JSUnusedLocalSymbols
             await async.waterfall([
-                    (cb) => // first confirm there are no records
-                        request
-                            .get('/4_0_0/ExplanationOfBenefit')
-                                .set(getHeaders())
-                            .expect(200, (err, resp) => {
-                                expect(resp.body.length).toBe(0);
-                                console.log('------- response 1 ------------');
-                                console.log(JSON.stringify(resp.body, null, 2));
-                                console.log('------- end response 1 ------------');
-                                return cb(err, resp);
-                            }),
-                    (results, cb) =>
-                        request
-                            .post('/4_0_0/ExplanationOfBenefit/1/$merge')
-                            .send(explanationOfBenefitBundleResource)
-                                .set(getHeaders())
-                            .expect(200, (err, resp) => {
-                                console.log('------- response 2 ------------');
-                                console.log(JSON.stringify(resp.body, null, 2));
-                                console.log('------- end response 2  ------------');
-                                return cb(err, resp);
-                            }),
-                    (results, cb) => request
+                (cb) => // first confirm there are no records
+                    request
                         .get('/4_0_0/ExplanationOfBenefit')
-                                .set(getHeaders())
-                        .expect(200, cb)
-                        .expect((resp) => {
-                            // clear out the lastUpdated column since that changes
-                            let body = resp.body;
-                            console.log('------- response 5 ------------');
+                        .set(getHeaders())
+                        .expect(200, (err, resp) => {
+                            expect(resp.body.length).toBe(0);
+                            console.log('------- response 1 ------------');
                             console.log(JSON.stringify(resp.body, null, 2));
-                            console.log('------- end response 5  ------------');
-                            expect(body.length).toBe(2);
-                            body.forEach(element => {
+                            console.log('------- end response 1 ------------');
+                            return cb(err, resp);
+                        }),
+                (results, cb) =>
+                    request
+                        .post('/4_0_0/ExplanationOfBenefit/1/$merge')
+                        .send(explanationOfBenefitBundleResource)
+                        .set(getHeaders())
+                        .expect(200, (err, resp) => {
+                            console.log('------- response 2 ------------');
+                            console.log(JSON.stringify(resp.body, null, 2));
+                            console.log('------- end response 2  ------------');
+                            return cb(err, resp);
+                        }),
+                (results, cb) => request
+                    .get('/graphql/?query={ explanationOfBenefits { id patient { id name { family } } } }')
+                    .set(getHeaders())
+                    .expect(200, cb)
+                    .expect((resp) => {
+                        // clear out the lastUpdated column since that changes
+                        let body = resp.body;
+                        console.log('------- response graphql ------------');
+                        console.log(JSON.stringify(resp.body, null, 2));
+                        console.log('------- end response graphql  ------------');
+                        expect(body.data.explanationOfBenefits.length).toBe(3);
+                        let expected = expectedExplanationOfBenefitBundleResource;
+                        expected.forEach(element => {
+                            if ('meta' in element) {
                                 delete element['meta']['lastUpdated'];
-                            });
-                            let expected = expectedExplanationOfBenefitBundleResource;
-                            expected.forEach(element => {
-                                if ('meta' in element) {
-                                    delete element['meta']['lastUpdated'];
-                                }
-                                // element['meta'] = {'versionId': '1'};
-                                if ('$schema' in element) {
-                                    delete element['$schema'];
-                                }
-                            });
-                            expect(body).toStrictEqual(expected);
-                        }, cb),
-                ]);
+                            }
+                            // element['meta'] = {'versionId': '1'};
+                            if ('$schema' in element) {
+                                delete element['$schema'];
+                            }
+                        });
+                        expect(body.data.explanationOfBenefits).toStrictEqual(expected);
+                    }, cb),
+            ]);
         });
     });
 });
