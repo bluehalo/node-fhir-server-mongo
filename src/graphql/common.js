5192eb3a497f3bac0fe3ca51cd48c564858c13ea
imranq2 <imranq2@hotmail.com>
2021-09-28 18:49:14 -0700

    create union for multiple reference targets

--- src/graphql/common.js
+++ src/graphql/common.js
@@ -1,3 +1,24 @@
+/**
+ * This functions takes a FHIR Bundle and returns the resources in it
+ * @param bundle
+ * @return {Resource}
+ */
 module.exports.unBundle = (bundle) => {
     return bundle.entry.map(e => e.resource);
+};
+
+// noinspection JSUnusedLocalSymbols
+/**
+ * This is to handle unions in GraphQL
+ * @param obj
+ * @param context
+ * @param info
+ * @return {null|*}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports.resolveType = (obj, context, info) => {
+    if (obj) {
+        return obj.resourceType;
+    }
+    return null; // GraphQLError is thrown
 };
