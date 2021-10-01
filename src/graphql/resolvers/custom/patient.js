be7ad3891b65bccd633785f3e3907e9db5f0cd64
Hundley Crews <hcrews@gmail.com>
2021-10-01 11:53:19 -0400

    Add allergyIntolerance reverse link to patient.

--- src/graphql/resolvers/custom/patient.js
+++ src/graphql/resolvers/custom/patient.js
@@ -15,5 +15,18 @@ module.exports = {
                 'ExplanationOfBenefit'
             );
         },
+        // eslint-disable-next-line no-unused-vars
+        allergyIntolerance: async (parent, args, context, info) => {
+            return await getResources(
+                parent,
+                {
+                    ...args,
+                    'patient': parent.id,
+                },
+                context,
+                info,
+                'AllergyIntolerance'
+            );
+        },
     },
 };
