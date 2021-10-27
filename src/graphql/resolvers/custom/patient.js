705db986e8ff070f27ac7f72fee7b443b9fe1979
rajanlal88 <rajan.lal@icanbwell.com>
2021-10-27 18:48:51 +0530

    HD-133 Schema update for condition

--- src/graphql/resolvers/custom/patient.js
+++ src/graphql/resolvers/custom/patient.js
@@ -28,5 +28,18 @@ module.exports = {
                 'AllergyIntolerance'
             );
         },
+        // eslint-disable-next-line no-unused-vars
+        condition: async (parent, args, context, info) => {
+            return await getResources(
+                parent,
+                {
+                    ...args,
+                    'patient': parent.id,
+                },
+                context,
+                info,
+                'Condition'
+            );
+        },
     },
 };
