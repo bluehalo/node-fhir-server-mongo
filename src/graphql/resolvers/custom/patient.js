65a4aa7af6cca60588f9f3e4c21bd5919d7816a3
rajanlal88 <rajan.lal@icanbwell.com>
2021-10-28 17:31:37 +0530

    HD-106 Schema update for careplan

--- src/graphql/resolvers/custom/patient.js
+++ src/graphql/resolvers/custom/patient.js
@@ -28,5 +28,18 @@ module.exports = {
                 'AllergyIntolerance'
             );
         },
+        // eslint-disable-next-line no-unused-vars
+        carePlan: async (parent, args, context, info) => {
+            return await getResources(
+                parent,
+                {
+                    ...args,
+                    'patient': parent.id,
+                },
+                context,
+                info,
+                'CarePlan'
+            );
+        },
     },
 };
