2c42b9e448926de1f144970599724533ca8117bb
Michael Booth <61957561+michaelrbooth@users.noreply.github.com>
2021-12-11 12:02:59 -0800

    fix formatting

--- src/graphql/resolvers/custom/patient.js
+++ src/graphql/resolvers/custom/patient.js
@@ -2,60 +2,60 @@ const { getResources } = require('../../common');
 const { merge } = require('../../../operations/merge/merge');
 
 module.exports = {
-  Patient: {
-    // eslint-disable-next-line no-unused-vars
-    explanationOfBenefit: async (parent, args, context, info) => {
-        return await getResources(
-            parent,
-            {
-                ...args,
-                'patient': parent.id,
-            },
-            context,
-            info,
-            'ExplanationOfBenefit'
-        );
-    },
-    // eslint-disable-next-line no-unused-vars
-    allergyIntolerance: async (parent, args, context, info) => {
-        return await getResources(
-            parent,
-            {
-                ...args,
-                'patient': parent.id,
-            },
-            context,
-            info,
-            'AllergyIntolerance'
-        );
-    },
-    // eslint-disable-next-line no-unused-vars
-    condition: async (parent, args, context, info) => {
-        return await getResources(
-            parent,
-            {
-                ...args,
-                'patient': parent.id,
-            },
-            context,
-            info,
-            'Condition'
-        );
-    },
-    // eslint-disable-next-line no-unused-vars
-    carePlan: async (parent, args, context, info) => {
-        return await getResources(
-            parent,
-            {
-                ...args,
-                'patient': parent.id,
-            },
-            context,
-            info,
-            'CarePlan'
-        );
-    },
-},
+    Patient: {
+      // eslint-disable-next-line no-unused-vars
+      explanationOfBenefit: async (parent, args, context, info) => {
+          return await getResources(
+              parent,
+              {
+                  ...args,
+                  'patient': parent.id,
+              },
+              context,
+              info,
+              'ExplanationOfBenefit'
+          );
+      },
+      // eslint-disable-next-line no-unused-vars
+      allergyIntolerance: async (parent, args, context, info) => {
+          return await getResources(
+              parent,
+              {
+                  ...args,
+                  'patient': parent.id,
+              },
+              context,
+              info,
+              'AllergyIntolerance'
+          );
+      },
+      // eslint-disable-next-line no-unused-vars
+      condition: async (parent, args, context, info) => {
+          return await getResources(
+              parent,
+              {
+                  ...args,
+                  'patient': parent.id,
+              },
+              context,
+              info,
+              'Condition'
+          );
+      },
+      // eslint-disable-next-line no-unused-vars
+      carePlan: async (parent, args, context, info) => {
+          return await getResources(
+              parent,
+              {
+                  ...args,
+                  'patient': parent.id,
+              },
+              context,
+              info,
+              'CarePlan'
+          );
+      },
+  },
   Mutation: {
     updateGeneralPractitioner:
       // eslint-disable-next-line no-unused-vars
