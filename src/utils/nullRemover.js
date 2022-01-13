52762f46dcf971b4a69fc40361e77d124f7a71d1
imranq2 <imranq2@hotmail.com>
2022-01-12 19:55:35 -0800

    fix tests

--- src/utils/nullRemover.js
+++ src/utils/nullRemover.js
@@ -1,9 +1,14 @@
+/**
+ * This function removes any null, undefined, empty objects and empty arrays
+ * @param {Object} obj
+ * @return {Object}
+ */
 const removeNull = (obj) => {
     Object.keys(obj).forEach(key => {
             // Get this value and its type
-            const value = obj[key];
+            const value = obj[`${key}`];
             if (value === null) {
-                delete obj[key];
+                delete obj[`${key}`];
                 return;
             }
             const type = typeof value;
@@ -11,19 +16,18 @@ const removeNull = (obj) => {
                 // Recurse...
                 removeNull(value);
                 if (!Object.keys(value).length) {
-                    delete obj[key];
+                    delete obj[`${key}`];
                 }
                 if (Array.isArray(value)) {
                     if (value.length === 0) {
-                        delete obj[key];
+                        delete obj[`${key}`];
                     }
                     for (const arrayItem of value) {
                         removeNull(arrayItem);
                     }
                 }
             } else if (type === 'undefined') {
-                // Undefined, remove it
-                delete obj[key];
+                delete obj[`${key}`];
             }
         }
     );
