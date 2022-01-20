212f4846a8f8ed40d31218ed9f4206a774ca8ffb
imranq2 <imranq2@hotmail.com>
2022-01-20 15:52:57 -0800

    remove extraneous logging

--- src/utils/prometheus.utils.js
+++ src/utils/prometheus.utils.js
@@ -83,7 +83,7 @@ module.exports.requestCounters = function (req, res, next) {
 const responseCounters = responseTime(function (req, res, time) {
     if (req.url !== '/metrics') {
         responses.labels(req.method, req.path, res.statusCode).observe(time);
-        console.info('res.StatusCode=' + res.statusCode);
+        // console.info('res.StatusCode=' + res.statusCode);
         if (res.statusCode === 404) {
             validationsFailed.inc(1);
         }
