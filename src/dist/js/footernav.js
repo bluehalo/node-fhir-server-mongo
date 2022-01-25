511043c63f2de7d011ab1b08f85c52b3364e258f
bwell-bruce <95768590+bwell-bruce@users.noreply.github.com>
2022-01-25 16:38:51 -0700

    Fixed elementid and added disabled logic for prev button (#362)

--- src/dist/js/footernav.js
+++ src/dist/js/footernav.js
@@ -1,28 +1,29 @@
-document.getElementById("lnkNext").addEventListener("click", function () {
+const url = new URL(window.location);
+const currentPage = url.searchParams.get("_getpagesoffset");
+
+const nextLink = document.getElementById("lnkNext");
+nextLink.addEventListener("click", function () {
     goNext();
 }, false);
 
-document.getElementById("lnkNext").addEventListener("click", function () {
+const prevLink = document.getElementById("lnkPrevious");
+prevLink.addEventListener("click", function () {
     goPrevious();
 }, false);
 
+if(!currentPage || currentPage === "0"){
+    prevLink.closest(".page-item").classList.add("disabled");
+} else {
+    prevLink.closest(".page-item").classList.remove("disabled");
+}
+
 function goNext() {
-    const url = new URL(window.location);
-    const currentPage = url.searchParams.get("_getpagesoffset");
-    if (currentPage) {
-        const nextPage = parseInt(currentPage) + 1;
-        url.searchParams.set("_getpagesoffset", nextPage);
-    }
-    else {
-        const nextPage = 1;
-        url.searchParams.set("_getpagesoffset", nextPage);
-    }
+    const nextPage = currentPage ? parseInt(currentPage) + 1 : 1;
+    url.searchParams.set("_getpagesoffset", nextPage);
     url.searchParams.set("_count", 10);
     window.location.assign(url);
 }
 function goPrevious() {
-    const url = new URL(window.location);
-    const currentPage = url.searchParams.get("_getpagesoffset");
     if (currentPage) {
         const nextPage = parseInt(currentPage) - 1;
         if (nextPage >= 0) {
