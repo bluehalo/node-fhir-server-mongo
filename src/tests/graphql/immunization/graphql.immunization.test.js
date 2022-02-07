5e9f2f9c00ace30c29b54b2091f273123934d99a
Imran Qureshi <imranq2@hotmail.com>
2022-02-07 07:26:18 -0800

    fix bug with date query when the FHIR search parameter has multiple fields (#378)

---
+++ src/tests/graphql/immunization/graphql.immunization.test.js
@@ -0,0 +1,113 @@
+/* eslint-disable no-unused-vars */
+const supertest = require('supertest');
+
+const {app} = require('../../../app');
+const immunizationBundleResource = require('./fixtures/immunization.json');
+const expectedGraphQLResponse = require('./fixtures/expected_graphql_response.json');
+
+const patientBundleResource = require('./fixtures/patients.json');
+
+const fs = require('fs');
+const path = require('path');
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const explanationOfBenefitQuery = fs.readFileSync(path.resolve(__dirname, './fixtures/query.graphql'), 'utf8');
+
+const request = supertest(app);
+const {
+    commonBeforeEach,
+    commonAfterEach,
+    getHeaders,
+    getGraphQLHeaders
+} = require('../../common');
+
+describe('GraphQL ExplanationOfBenefit Tests', () => {
+    beforeEach(async () => {
+        await commonBeforeEach();
+    });
+
+    afterEach(async () => {
+        await commonAfterEach();
+    });
+
+    describe('GraphQL ExplanationOfBenefit', () => {
+        test('GraphQL ExplanationOfBenefit properly', async () => {
+            // noinspection JSUnusedLocalSymbols
+            const graphqlQueryText = explanationOfBenefitQuery.replace(/\\n/g, '');
+            let resp = await request
+                .get('/4_0_0/ExplanationOfBenefit')
+                .set(getHeaders())
+                .expect(200);
+            expect(resp.body.length).toBe(0);
+            console.log('------- response 1 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 1 ------------');
+
+            resp = await request
+                .post('/4_0_0/Patient/1/$merge')
+                .send(patientBundleResource)
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response 2 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 2  ------------');
+
+            resp = await request
+                .post('/4_0_0/Immunization/1/$merge')
+                .send(immunizationBundleResource)
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response 2 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 2  ------------');
+
+            resp = await request
+                .get('/4_0_0/Patient/')
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response patient ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response patient  ------------');
+
+            resp = await request
+                .get('/4_0_0/Immunization/')
+                .set(getHeaders())
+                .expect(200);
+            console.log('------- response 2 ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response 2  ------------');
+
+            resp = await request
+                // .get('/graphql/?query=' + graphqlQueryText)
+                // .set(getHeaders())
+                .post('/graphql')
+                .send({
+                    'operationName': null,
+                    'variables': {},
+                    'query': graphqlQueryText
+                })
+                .set(getGraphQLHeaders())
+                .expect(200);
+            // clear out the lastUpdated column since that changes
+            let body = resp.body;
+            console.log('------- response graphql ------------');
+            console.log(JSON.stringify(resp.body, null, 2));
+            console.log('------- end response graphql  ------------');
+            if (body.errors) {
+                console.log(body.errors);
+                expect(body.errors).toBeUndefined();
+            }
+            expect(body.data.immunization.length).toBe(1);
+            let expected = expectedGraphQLResponse;
+            expected.forEach(element => {
+                if ('meta' in element) {
+                    delete element['meta']['lastUpdated'];
+                }
+                if ('$schema' in element) {
+                    delete element['$schema'];
+                }
+            });
+            expect(body.data.immunization).toStrictEqual(expected);
+        });
+    });
+});
