3db79895244e9aebe2c7f5619b23dab15ea2ba71
imranq2 <imranq2@hotmail.com>
2022-02-27 11:20:23 -0800

    address comments

--- src/graphql/v2/dataSource.js
+++ src/graphql/v2/dataSource.js
@@ -4,44 +4,43 @@ const {getRequestInfo} = require('./requestInfoHelper');
 const {logWarn} = require('../../operations/common/logging');
 const async = require('async');
 const DataLoader = require('dataloader');
+const {groupBy} = require('../../utils/list.util');
+
 
 /**
- * https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
- * @param {*[]} sourceArray
- * @param key
- * @return {*}
+ * This class stores the tuple of resourceType and id to uniquely identify a resource
  */
-const groupBy = function (sourceArray, key) { // `sourceArray` is an array of objects, `key` is the key (or property accessor) to group by
-    // reduce runs this anonymous function on each element of `sourceArray` (the `item` parameter,
-    // returning the `storage` parameter at the end
-    return sourceArray.reduce(function (storage, item) {
-        // get the first instance of the key by which we're grouping
-        const group = item[`${key}`];
-
-        // set `storage` for this instance of group to the outer scope (if not empty) or initialize it
-        storage[`${group}`] = storage[`${group}`] || [];
-
-        // add this item to its group within `storage`
-        storage[`${group}`].push(item);
-
-        // return the updated storage to the reduce function, which will then loop through the next
-        return storage;
-    }, {}); // {} is the initial value of the storage
-};
-
 class ResourceWithId {
     constructor(resourceType, id) {
+        /**
+         * @type {string}
+         */
         this.resourceType = resourceType;
+        /**
+         * @type {string}
+         */
         this.id = id;
     }
 }
 
+/**
+ * This class implements the DataSource pattern so it is called by our GraphQL resolvers to load the data
+ */
 class FhirDataSource extends DataSource {
+    /**
+     * @param {RequestInfo} requestInfo
+     */
     constructor(requestInfo) {
         super();
+        /**
+         * @type {RequestInfo}
+         */
         this.requestInfo = requestInfo;
+        /**
+         * @type {DataLoader<unknown, {resourceType: string, id: string}, Resource>}
+         */
         this.dataLoader = new DataLoader(
-            async (keys) => await this.myBatchGetUsers(keys, requestInfo)
+            async (keys) => await this.getResourcesInBatch(keys, requestInfo)
         );
         this.meta = [];
     }
@@ -51,7 +50,7 @@ class FhirDataSource extends DataSource {
     }
 
     /**
-     * This functions takes a FHIR Bundle and returns the resources in it
+     * This function takes a FHIR Bundle and returns the resources in it
      * @param {Resource[]|{entry: {resource: Resource}[]}} bundle
      * @return {Resource[]}
      */
@@ -63,7 +62,8 @@ class FhirDataSource extends DataSource {
     }
 
     /**
-     * This functions takes a FHIR Bundle and returns the resources in it
+     * This function orders the resources by key so DataLoader can find the right results
+     * https://github.com/graphql/dataloader#batching
      * @param {{Resource}[]} resources
      * @param {ResourceWithId[]} keys
      * @return {Resource[]}
@@ -81,13 +81,13 @@ class FhirDataSource extends DataSource {
     }
 
     /**
-     * batch
+     * gets resources for the passed in keys
      * https://github.com/graphql/dataloader#batching
      * @param {ResourceWithId[]} keys
      * @param requestInfo
      * @return {Promise<Resource[]|{entry: {resource: Resource}[]}>}
      */
-    async myBatchGetUsers(keys, requestInfo) {
+    async getResourcesInBatch(keys, requestInfo) {
         // separate by resourceType
         /**
          * Each field in the object is the key
@@ -96,14 +96,16 @@ class FhirDataSource extends DataSource {
         const groupKeysByResourceType = groupBy(keys, 'resourceType');
         // noinspection UnnecessaryLocalVariableJS
         /**
-         * @type {*|Promise<unknown>}
+         * @type {Resource[]}
          */
         const results = this.reorderResources(
+            // run the loads in parallel by resourceType
             await async.flatMap(Object.entries(groupKeysByResourceType), async groupKeysByResourceTypeKey => {
-                /**
-                 * @type {ResourceWithId}
-                 */
+                // resourceType is a string and resources is a list of resources of that resourceType
                 const [resourceType, resources] = groupKeysByResourceTypeKey;
+                /**
+                 * @type {String}
+                 */
                 const idOfReference = resources.map(r => r.id).join(',');
                 return this.unBundle(
                     await search(
@@ -135,16 +137,15 @@ class FhirDataSource extends DataSource {
     // noinspection JSUnusedLocalSymbols
     // eslint-disable-next-line no-unused-vars
     resolveType(obj, context, info) {
-        if (Array.isArray(obj)) {
-            if (obj.length > 0) {
-                // apollo does not seem to allow returning lists
-                // return obj.map(o => resolveType(o, context, info));
-                return obj[0].resourceType;
-            } else {
-                return null;
-            }
-        } else {
+        if (!Array.isArray(obj)) {
             return obj.resourceType;
+        }
+        if (obj.length > 0) {
+            // apollo does not seem to allow returning lists.  Uncomment when Apollo supports that.
+            // return obj.map(o => resolveType(o, context, info));
+            return obj[0].resourceType;
+        } else {
+            return null;
         }
     }
 
@@ -162,13 +163,22 @@ class FhirDataSource extends DataSource {
             return null;
         }
         /**
-         * @type {string}
+         * @type {string[]}
          */
-        const typeOfReference = reference.reference.split('/')[0];
+        const references = reference.reference.split('/');
+        if (references.length !== 2) {
+            return null;
+        }
         /**
+         * first part of the reference is the resourceType
          * @type {string}
          */
-        const idOfReference = reference.reference.split('/')[1];
+        const typeOfReference = references[0];
+        /**
+         * second part is the id of the resource
+         * @type {string}
+         */
+        const idOfReference = references[1];
         try {
             return await this.dataLoader.load(
                 new ResourceWithId(
@@ -180,6 +190,8 @@ class FhirDataSource extends DataSource {
             if (e.name === 'NotFound') {
                 logWarn(context.user, `findResourceByReference: Resource ${typeOfReference}/${idOfReference} not found for parent:${parent.resourceType}/${parent.id} `);
                 return null;
+            } else {
+                throw e;
             }
         }
     }
@@ -198,29 +210,9 @@ class FhirDataSource extends DataSource {
             return null;
         }
         return async.flatMap(references, async reference => {
-            /**
-             * @type {string}
-             */
-            const typeOfReference = reference.reference.split('/')[0];
-            /**
-             * @type {string}
-             */
-            const idOfReference = reference.reference.split('/')[1];
-            try {
-                return await this.dataLoader.load(
-                    new ResourceWithId(
-                        typeOfReference,
-                        idOfReference
-                    )
-                );
-            } catch (e) {
-                if (e.name === 'NotFound') {
-                    logWarn(context.user, `findResourcesByReference: Resource ${typeOfReference}/${idOfReference} not found for parent:${parent.resourceType}/${parent.id}`);
-                    return null;
-                } else {
-                    throw e;
-                }
-            }
+            return await this.findResourceByReference(
+                parent, args, context, info, reference
+            );
         });
     }
 
