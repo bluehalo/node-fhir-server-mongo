424cc5d1bafcda09cec5babdb6b77ae7e0660542
imranq2 <imranq2@hotmail.com>
2022-02-27 17:52:31 -0800

    fix test

--- src/tests/graphql/explanationOfBenefit/graphql.explanationOfBenefits.test.js
+++ src/tests/graphql/explanationOfBenefit/graphql.explanationOfBenefits.test.js
@@ -118,7 +118,7 @@ describe('GraphQL ExplanationOfBenefit Tests', () => {
                 console.log(body.errors);
                 expect(body.errors).toBeUndefined();
             }
-            assertCompareBundles(body.data.explanationOfBenefit, expectedGraphQLResponse[0]);
+            assertCompareBundles(body.data.explanationOfBenefit, expectedGraphQLResponse[0], true);
         });
     });
 });
