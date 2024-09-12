c9cd5caa8cfc712c20ff79e3f1aa0c60bf4719c3
eugene.koon <eugene.koon@bluehalo.com>
2024-09-12 09:37:16 -0400

    update github lint and test

---
+++ scripts/curlRequests/patient_get_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+URL="http://localhost:3000/4_0_0/Patient/example"
+
+# Perform the curl request and capture the HTTP status code
+response=$(curl -s -o response.log -w "%{http_code}" -X GET "$URL" \
+  -H "Content-Type: application/fhir+json" \
+  -H "Cache-Control: no-cache" )
+
+# Print the HTTP status code
+echo "HTTP Status Code: $response"
+
+# Check if the status code is 200
+if [ "$response" -ne 200 ]; then
+  echo "Request failed with status code $response"
+  exit 1
+fi
+
+echo "Request succeeded with status code $response"
